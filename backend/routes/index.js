7aaa1e3f03a15697bac56851ae7c6671e7ed5a31
LCastel <louis.castel34@icloud.com>
2022-02-22 17:27:00 +0100

    init

---
+++ backend/routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.use('/api/user', (req,res)=>{
+    res.send("ok");
+});
+
+moduule.exports =router;
---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const routing = require('./backend/routes');
+const port = process.env.PORT || 5001;
+
+exports.app= app;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+app.use(routing);
+
+app.use(express.static(path.join(__dirname, "./frontend/build")));
+app.get("*",function(_, res){
+    res.sendFile(
+        path.join(__dirname, "./frontend/build:index.html"),
+        function(err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    )
+});
+
+app.listen(port, ()=>console.log(`Server Runing on port ${port}`));
+
+module.exports = app;
