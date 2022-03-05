21f170163d17e019a1de58db63f0142bec7e1891
LCastel <louis.castel34@icloud.com>
2022-03-05 16:56:17 +0100

    fix post 232

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(routing);
 app.use(express.static(path.join(__dirname, "./frontend/build")));
 app.get("*",function(_, res){
     res.sendFile(
-        path.join(__dirname, "./frontend/build:index.html"),
+        path.join(__dirname, "./frontend/build/index.html"),
         function(err){
             if(err){
                 res.status(500).send(err);
